c06fc0d31b0cd9f36f2bdee4ba70bd18ca67e275
Firdavs <firdavs20fe@gmail.com>
2025-03-15 09:00:01 +0300

    refactor: adjust ANSI code buffer length checks in SteamPrint function
    - Updated conditions in the `SteamPrint` function to increase the buffer length thresholds for processing ANSI codes, enhancing the handling of incorrectly formatted ANSI sequences.
    - Improved logic for processing ANSI codes to ensure better output management and display.

--- utils/steam_print.go
+++ utils/steam_print.go
@@ -12,14 +12,14 @@ func SteamPrint(text string, ansiBuffer *string) {
 		processedBuffer := ProcessANSICodes(*ansiBuffer)
 		fmt.Print(processedBuffer)
 		*ansiBuffer = ""
-	} else if config.AnsiStartPattern.MatchString(*ansiBuffer) && len(*ansiBuffer) > 30 {
+	} else if config.AnsiStartPattern.MatchString(*ansiBuffer) && len(*ansiBuffer) > 100 {
 		// If buffer contains the start of an ANSI code, but not the end
 		// and buffer length is more than 30, process it
 		// This can happen when ANSI code is in incorrect format
 		processedBuffer := ProcessANSICodes(*ansiBuffer)
 		fmt.Print(processedBuffer)
 		*ansiBuffer = ""
-	} else if len(*ansiBuffer) > 20 && !config.AnsiStartPattern.MatchString(*ansiBuffer) {
+	} else if len(*ansiBuffer) > 90 && !config.AnsiStartPattern.MatchString(*ansiBuffer) {
 		// If buffer length is more than 20 and no ANSI code start is found,
 		// process it
 		processedBuffer := ProcessANSICodes(*ansiBuffer)
